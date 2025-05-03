a284170d3af6fa26a9f4947b9f3532196a929582
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-05-03 21:36:13 +0200

    Update equipements.js

--- assets/js/equipements.js
+++ assets/js/equipements.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       popup.style.cssText = `
         display: none; position: fixed; top: 0; left: 0;
         width: 100%; height: 100%; z-index: 9999;
-        background: rgba(0,0,0,0.6); justify-content: center; align-items: center;
+        background: rgba(255,255,255,0.5); justify-content: center; align-items: center;
       `;
 
       popup.innerHTML = `
