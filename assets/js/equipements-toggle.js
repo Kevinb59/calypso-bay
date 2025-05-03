1a6416b99860bb9f63ed57ba13a07d642a179252
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-05-03 14:57:13 +0200

    Update equipements-toggle.js

--- assets/js/equipements-toggle.js
+++ assets/js/equipements-toggle.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".toggle-content").forEach(el => {
+    el.style.display = "none"; // tout fermé par défaut
+  });
+
   document.querySelectorAll(".toggle-btn").forEach((button) => {
     button.addEventListener("click", () => {
       const content = button.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
+      content.style.display = content.style.display === "block" ? "none" : "block";
     });
   });
 });
