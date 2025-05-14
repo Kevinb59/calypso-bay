e986d5b9d09e860895666ef6c9f5f888ab60a940
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-05-14 23:09:56 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,15 +1,18 @@
 function initMap() {
-  const location = { lat: 16.108471, lng: -61.776106 };
+  const location = { lat: 16.1520569, lng: -61.7687748 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: location,
-    disableDefaultUI: false
+    disableDefaultUI: false,
+    mapTypeControl: true,
+    streetViewControl: true
   });
 
   new google.maps.Marker({
     position: location,
     map: map,
-    title: "Calypso Bay"
+    title: "Calypso Bay",
+    animation: google.maps.Animation.DROP
   });
 }
