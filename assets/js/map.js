1fc7f4aeba3522bb1df63fdd496dedb942a24d4c
Kevinb59 <130929760+Kevinb59@users.noreply.github.com>
2025-05-15 08:23:56 +0200

    Update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,30 @@
 function initMap() {
   const location = { lat: 16.1520569, lng: -61.7687748 };
 
+  const styledMap = new google.maps.StyledMapType(
+    [
+      {
+        featureType: "poi",
+        stylers: [{ visibility: "off" }]
+      }
+    ],
+    { name: "Sans POI" }
+  );
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
     center: location,
     disableDefaultUI: false,
     mapTypeControl: true,
-    streetViewControl: false
+    streetViewControl: false,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DEFAULT,
+      position: google.maps.ControlPosition.TOP_LEFT
+    }
   });
+
+  map.mapTypes.set("styled_map", styledMap);
+  map.setMapTypeId("styled_map");
 
   new google.maps.Marker({
     position: location,
